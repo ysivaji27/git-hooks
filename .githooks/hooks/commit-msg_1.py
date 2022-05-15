2aa26dee5104e5d1bbc62c373bfb76ec3172b22b
sivajiyelugoti <sivaji.y@gmail.com>
2022-05-15 18:38:10 +0100

    test

---
+++ .githooks/hooks/commit-msg_1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import sys
+def main()
+   print("Number Of Arguments: %d",len(sys.argv))
+   print("Arguments List %s" % str(sys.argv))
+
+   sys.exit(1)
+
+if _name_ == "_main_":
+   main()
